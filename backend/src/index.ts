1bf4cae4199745dc929a138d5950c211913f4260
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2023-09-24 21:17:44 +0100

    Allowed origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,7 @@ app.get('/', (req: Request, res: Response) => {
 app.use(express.json())
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: '*'
 }))
 
 const api = express.Router()
