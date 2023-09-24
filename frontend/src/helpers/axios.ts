9651325208f2e7814411995f1841aeb909a98c41
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2023-09-24 23:59:38 +0100

    Fixing auth issues

--- frontend/src/helpers/axios.ts
+++ frontend/src/helpers/axios.ts
@@ -3,6 +3,9 @@ import axios from 'axios'
 
 const client = axios.create({
 	baseURL: process.env.REACT_APP_SERVER_URL,
+	headers: {
+		Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('token') ?? '')
+	}
 })
 
 client.interceptors.response.use(function (response) {
