e18a202a803fce102e86e839699fa24b8647737c
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2023-09-25 02:00:01 +0100

    A backend fix and summary improvements

--- frontend/src/components/CarSummary/index.tsx
+++ frontend/src/components/CarSummary/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Car } from '../../types/cars'
 
-import { Typography } from 'antd'
+import { Tag, Typography } from 'antd'
 
 const { Text } = Typography
 
@@ -12,11 +12,11 @@ type Props = {
 function CarSummary({car: {make, colour, code}}: Props) {
 	return (
 		<div>
-			<Text>I have a {make?.name} and the colour is {colour?.name}</Text>
+			<Text>I have a <b>{make?.name}</b> and the colour is <Tag color={colour?.name ?? 'white'}>{colour?.name}</Tag>.</Text>
 
 			{colour?.id == Number(process.env.REACT_APP_SPECIAL_COLOUR_ID) && <div style={{textTransform: 'uppercase'}}><br/><Text strong>THIS CAR IS {colour?.name}! NICE!!</Text></div> }
 
-			<br /><Text>REF: {code}</Text>
+			{code && <><br /><Text>REF: <u>{code}</u></Text></>}
 		</div>
 	)
 }
