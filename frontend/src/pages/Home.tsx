1bcbcda1fe1aae90188a784b605eebc7b8f3c09a
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2023-09-25 02:56:11 +0100

    Improved Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,23 @@
+import { Typography } from 'antd'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const { Title } = Typography
 
 function Home() {
-	return (<nav>
-		<ul>
-			<li><Link to='/cars'>Cars</Link></li>
-		</ul>
-	</nav>)
+	return (
+		<div>
+			<Title>Welcome to Green Cars</Title>
+			<Typography>
+				Please click on the following link:
+			</Typography>
+			<nav>
+				<ul>
+					<li><Link to='/cars'>Cars</Link></li>
+				</ul>
+			</nav>
+		</div>
+	)
 }
 
 export default Home
